d42100813992802466c24c62487dc614dcb3d646
Ivan Shevtsov <vanyashevcov@ukr.net>
2025-05-01 19:24:14 +0300

    Fixed

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,7 +4,7 @@ import createHttpError from "http-errors";
 export const isValidId = (req, res, next) => {
     const { id } = req.params;
     if (!isValidObjectId(id)) {
-        return next(createHttpError(404, `${id} not valid id`));
+        return next(createHttpError(400, `${id} not valid id`));
     }
     next();
 };
