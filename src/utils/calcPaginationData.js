b3aea761b7d9711aaef5132eec503735f1d12426
Ivan Shevtsov <vanyashevcov@ukr.net>
2025-05-02 17:53:12 +0300

    Fixed totalItems

--- src/utils/calcPaginationData.js
+++ src/utils/calcPaginationData.js
@@ -2,5 +2,5 @@ export const calcPaginationData = ({ page, perPage, totalItems }) => {
     const totalPages = Math.ceil(totalItems / perPage);
     const hasPreviousPage = page > 1;
     const hasNextPage = page < totalPages;
-    return { totalPages, hasPreviousPage, hasNextPage };
+    return { totalPages, hasPreviousPage, hasNextPage, totalItems };
 };
