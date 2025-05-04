96dc368aa837af89befcca242e4bd79cb161dd4d
Ivan Shevtsov <vanyashevcov@ukr.net>
2025-05-04 16:18:13 +0300

    Fixed

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -2,17 +2,17 @@ import createHttpError from 'http-errors';
 import { findSession, findUser } from '../services/auth.js';
 
 export const authenticate = async (req, res, next) => {
-  const authorization = req.get('Authorization');
-  if (!authorization) {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
     return next(createHttpError(401, 'Authorization header missing'));
   }
 
-  const [bearer, accessToken] = authorization.split(' ');
-  if (bearer !== 'Bearer') {
+  const [bearer, token] = authHeader.split(' ');
+  if (bearer !== 'Bearer' || !token) {
     return next(createHttpError(401, 'Header must have type Bearer'));
   }
 
-  const session = await findSession({ accessToken });
+  const session = await findSession({ accessToken: token });
   if (!session) {
     return next(createHttpError(401, 'Session not found'));
   }
@@ -20,13 +20,13 @@ export const authenticate = async (req, res, next) => {
   if (session.accessTokenValidUntil < Date.now()) {
     return next(createHttpError(401, 'Access token expired'));
   }
-  
-    const user = await findUser({ _id: session.userId });
-    if (!user) {
-      return next(createHttpError(401, 'User not found'));
-    }
 
-    req.user = user;
+  const user = await findUser({ _id: session.userId });
+  if (!user) {
+    return next(createHttpError(401, 'User not found'));
+  }
 
-    next();
+  req.user = user;
+
+  next();
 };
