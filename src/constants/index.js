383765cec67fefb8983a0507082ceef88149ab82
Ivan Shevtsov <vanyashevcov@ukr.net>
2025-05-10 10:16:56 +0300

    Fixed

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,9 @@ export const sortList = ['asc', 'desc'];
 
 export const TEMPLATES_DIR = path.resolve('src', 'templates');
 
-export const TEMPORARY_FILE_DIR = path.resolve('temp');
+// export const TEMPORARY_FILE_DIR = path.resolve('temp');
+
+export const TEMPORARY_FILE_DIR = '/tmp';
+
 
 export const UPLOAD_FILE_DIR = path.resolve('upload');
