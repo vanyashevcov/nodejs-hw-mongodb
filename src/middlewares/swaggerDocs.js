01c7b5632dd26794a0563366d06abcce02cff059
Ivan Shevtsov <vanyashevcov@ukr.net>
2025-05-11 11:02:54 +0300

    Fixed swagger

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,16 +1,35 @@
-import { readFileSync } from 'node:fs';
+import { readFileSync, existsSync  } from 'node:fs';
 import swaggerUI from 'swagger-ui-express';
 
 import { SWAGGER_PATH } from '../constants/index.js';
 
+// export const swaggerDocs = () => {
+//   try {
+//     const swaggerDoc = JSON.parse(readFileSync(SWAGGER_PATH).toString());
+//     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+//   } catch {
+//     return (req, res) => {
+//       res.status(500).json({
+//         message: 'Can`t load swagger docs',
+//       });
+//     };
+//   }
+// };
+
 export const swaggerDocs = () => {
   try {
-    const swaggerDoc = JSON.parse(readFileSync(SWAGGER_PATH).toString());
+    if (!existsSync(SWAGGER_PATH)) {
+      throw new Error(`Swagger file not found at ${SWAGGER_PATH}`);
+    }
+
+    const swaggerDoc = JSON.parse(readFileSync(SWAGGER_PATH, 'utf-8'));
     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-  } catch {
+  } catch (error) {
+    console.error('Swagger loading error:', error.message);
     return (req, res) => {
       res.status(500).json({
         message: 'Can`t load swagger docs',
+        error: error.message,
       });
     };
   }
